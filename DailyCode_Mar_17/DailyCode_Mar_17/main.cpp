dab60da9d9eeb64d9a7e5b06b478641034010359
game110pm <game110pm@605-110-STU18>
2017-03-17 12:59:45 -0600

    djnfnifkfkffdnjogblkm

---
+++ DailyCode_Mar_17/DailyCode_Mar_17/main.cpp
@@ -0,0 +1,22 @@
+#include <iostream>
+#include <string>
+
+using namespace std;
+
+
+int main() {
+
+
+
+	string words[3];
+
+	for (int i = 0; i > 3; i++) {
+		
+			cin >> words[i];
+		
+		}
+	if (words[2] == "shamrock") {
+		cout << "i'm to tired to say somethin nice, so f**k it." << endl;
+	}
+
+}
