5b02ebecc2ae3dc8f1dd4c44710d0825e230bed2
game110pm <game110pm@605-110-STU18>
2017-03-20 13:18:05 -0600

    Done

--- DailyCode_Mar20/DailyCode_Mar20/main.cpp
+++ DailyCode_Mar20/DailyCode_Mar20/main.cpp
@@ -12,19 +12,20 @@ int main() {
 
 
 	while (1) {
+		prime = true;
 		num++;
 		for (int i = 2; i < num; i++) {
 			if (num%i == 0)
 				prime = false;
 
 
+
+
+			if (prime == true)
+				cout << num << " is your first prime number after that." << endl;
+
+
 		}
-		
-		if (prime == true)
-			cout << num<<" is your first prime number after that." << endl;
-
-
-
 	}
 }
 
