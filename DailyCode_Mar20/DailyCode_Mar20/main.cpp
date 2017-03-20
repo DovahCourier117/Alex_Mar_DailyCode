ccc4148cb87a1477f749fd482a3f8a94242f5386
game110pm <game110pm@605-110-STU18>
2017-03-20 13:15:34 -0600

    Not done, just tired of this freaking assignment.

---
+++ DailyCode_Mar20/DailyCode_Mar20/main.cpp
@@ -0,0 +1,31 @@
+#include <iostream>
+
+using namespace std;
+
+int main() {
+
+	int num;
+	bool prime = false;
+
+	cout << "Give me number" << endl;
+	cin >> num;
+
+
+	while (1) {
+		num++;
+		for (int i = 2; i < num; i++) {
+			if (num%i == 0)
+				prime = false;
+
+
+		}
+		
+		if (prime == true)
+			cout << num<<" is your first prime number after that." << endl;
+
+
+
+	}
+}
+
+
